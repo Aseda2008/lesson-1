733ccf90b70eb351bdde42bc540f4eb40da1897d
Aseda2008 <asedanurdinova@gmail.com>
2025-04-30 23:21:12 +0600

    2025.04.30

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Calculator from './components/Calculator'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Product from './components/Product';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/calculator" element={<Calculator />} />
+          <Route path="/product" element={<Product/>} />
         </Routes>
         <Footer />
       </div>
